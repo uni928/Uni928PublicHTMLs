df3c1617f60cd652713f87c8e42f408ab2405e61
uni928 <116860752+uni928@users.noreply.github.com>
2026-07-07 15:07:27 +0900

    Update plugin4.js

--- Plugin/plugin4.js
+++ Plugin/plugin4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Click Guard Confirm Full
 // @namespace    uni928-click-guard
-// @version      1.2.1
+// @version      1.2.2
 // @description  広告・外部リンク・後から追加されたボタン・大きなスマホ広告風ボタンを押す前に確認する
 // @match        *://*/*
 // @run-at       document-start
